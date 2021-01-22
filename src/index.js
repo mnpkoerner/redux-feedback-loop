6faf35de46f6e6a5eacf1053706e24e87b123531
mnpkoerner <mnpkoerner@gmail.com>
2021-01-22 16:51:02 -0600

    reducers set up, dependencies installed, file tree built

--- src/index.js
+++ src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//set up redux
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+
+//reducers
+
+//holding all data in an object
+const feedbackReducer = (state = {}, action) => {
+    switch(action.type){
+        case 'FEELING':
+            return state.feeling = action.payload;
+        case 'UNDERSTANDING':
+            return state.feeling = action.payload;
+        case 'SUPPORT':
+            return state.support = action.payload;
+        case 'COMMENTS':
+            return state.comments = action.payload;
+    }
+}
+
+
+//wrap app in provider tag to access store
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
