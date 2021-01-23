685b2c52363ef839f3533a77a6b5b55d3c42de48
mnpkoerner <mnpkoerner@gmail.com>
2021-01-22 21:00:42 -0600

    client side post route

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -1,21 +1,32 @@
-import {useSelector} from 'react-redux';
-import {useHistory} from 'react-router-dom'
-import {useDispatch} from 'react-redux'
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import axios from 'axios'
 
 
 export default function Submit() {
-    const feedbackReducer = useSelector(store=>store.feedbackReducer)
+    const feedbackReducer = useSelector(store => store.feedbackReducer)
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const handleClick = () => {
+    //send in feedbackReducer for POST
+    const handleClick = (data) => {
         //needs to be posted to server
-
-        alert('sub it')
-        dispatch({type: 'RESET'})
-        history.push('/');
+        //
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: data
+        }).then((response) => {
+            dispatch({ type: 'RESET' })
+            history.push('/');
+        }).catch((error) => {
+            console.log(error)
+            alert('error sending data')
+        })
     }
-    return(
+
+    return (
         <div>
             <h1>Thanks for your feedback!</h1>
             <h4>How did you feel today?</h4>
@@ -26,7 +37,7 @@ export default function Submit() {
             <h5>{feedbackReducer.support} out of 5</h5>
             <h3>Additional feedback:</h3>
             <h5>{feedbackReducer.comments}</h5>
-            <button onClick={()=>handleClick()}>Submit</button>
+            <button onClick={() => handleClick(feedbackReducer)}>Submit</button>
         </div>
     )
 }
