82445798fdf2cb97af8a715d9898784644f1272d
mnpkoerner <mnpkoerner@gmail.com>
2021-01-22 21:42:10 -0600

    user can enter feedback, post feedback to server and database, user sees response message, user gets sent back to the home page to start again with redux values cleared

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.post('/', (req, res) => {
+    const feedback = [
+        Number(req.body.feeling), Number(req.body.understanding), Number(req.body.support), req.body.comments
+    ]
+    const queryText = `
+        INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+        VALUES ($1, $2, $3, $4);
+    `;
+    pool.query(queryText, feedback)
+        .then((response) => {
+            console.log(response);
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500)
+        })
+
+})
+
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+const feedbackRouter = require('./routes/feedback.router.js');
+app.use('/feedback', feedbackRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
